9571e061ed0646cfe71a10c96a0d5cc19d86cdb4
Luis <lusfelipea@gmail.com>
2022-09-01 17:32:02 -0300

    build: added bayut api

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import { baseURL, fetchApi } from '../utils/fetchApi';
+import Property from '../components/Property';
+
 const Banner = ({ 
   ImageURL, 
   purpose, 
@@ -25,9 +28,9 @@ const Banner = ({
   </Flex>
 )
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
-    <div>
+    <Box>
       <Banner 
         ImageURL="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
         purpose="Rent a Home"
@@ -38,6 +41,9 @@ export default function Home() {
         linkName="/search?purpose-for-rent"
         buttonText="Explore Renting"
       />
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
       <Banner 
         ImageURL="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
         purpose="Buy a Home"
@@ -48,6 +54,21 @@ export default function Home() {
         linkName="/search?purpose-for-sale"
         buttonText="Explore Buying"
       />
-    </div>
+      <Flex flexWrap="wrap">
+      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
+    </Box>
   )
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseURL}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    }
+  }
 }
