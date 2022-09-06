3668db316ee3a3f7838b32731b8540b58715ec87
Luis <lusfelipea@gmail.com>
2022-09-06 15:47:19 -0300

    feat: search filters added

--- components/searchFilters/index.jsx
+++ components/searchFilters/index.jsx
@@ -4,11 +4,50 @@ import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
 
+import { filterData, getFilterValues } from '../../utils/filterData'
+
 const SearchFilters = () => {
+    const [filters] = useState(filterData);
+
+    const searchProperties = (filterValues) => {
+        
+    }
+
     return (
-        <div>
-            SearchFilters
-        </div>
+        <Flex
+            bg="gray.100"
+            p="4"
+            justifyContent="center"
+            flexWrap="wrap"
+        >
+            {filters.map((filter) => {
+                return (           
+                    <Box 
+                        key={filter.queryName}
+                    >
+                        <Select
+                            w="fit-content"
+                            p="2"
+                            placeholder={filter.placeholder}
+                            onChange={
+                                (event) => searchProperties(
+                                    { [filter.queryName]: event.target.value, }
+                                )
+                            }
+                        >
+                            {filter?.items?.map((item) => (
+                                <option
+                                    value={item.value}
+                                    key={item.value}
+                                >
+                                    {item.name}
+                                </option>
+                            ))}
+                        </Select>
+                    </Box>
+                )
+            })}
+        </Flex>
     )
 }
 
